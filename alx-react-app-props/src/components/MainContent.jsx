05c6690e57ab3f59e96b8c0d0484d45377c5a0ce
Fredrick Murefu <freddymurefu@gmail.com>
2025-07-29 16:57:58 +0300

    Initial Commit

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return(
+        <main>
+  <h1 style={{ fontFamily: 'Trebuchet', color: 'navy', fontStyle: 'italic'}}>I love to visit New York, Paris, and Tokyo.</h1>
+</main>
+    )
+}
+
+export default MainContent
