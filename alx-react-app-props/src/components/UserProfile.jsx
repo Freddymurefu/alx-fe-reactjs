9be6690197a1b0e42f3f34c2e2237f70d67374b5
Fredrick Murefu <freddymurefu@gmail.com>
2025-07-29 17:51:01 +0300

    Edited Context Code in UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,16 +1,16 @@
-import { UseContext } from 'react'
+import { useContext } from 'react'
 import { UserContext } from "./UserContext"
 
 
 
 function UserProfile() {
-const { name, age, bio} = UseContext(UserContext)
+const { name, age, bio} = useContext(UserContext)
 
 return(
     <div style={{ border: '1px solid grey', fontFamily: 'Trebuchet', fontSize: '16px', margin: '10px'}}>
-        <h2 style={{ color: 'skyblue'}}>{props.name}</h2>
-        <p>Age: <span style={{ fontWeight: 'bold' }}>  {props.age} </span></p>
-        <p>Bio: {props.bio}</p>
+        <h2 style={{ color: 'skyblue'}}></h2>
+        <p>Age: <span style={{ fontWeight: 'bold' }}>  </span></p>
+        <p>Bio: </p>
     </div>
 )
 }
