0640ccd9a240e0702cc7ae73801faa659a0f4d11
Fredrick Murefu <freddymurefu@gmail.com>
2025-07-29 15:28:03 +0300

    Styled react Components Using Inline CSS Styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return(
         <main>
-  <h1>I love to visit New York, Paris, and Tokyo.</h1>
+  <h1 style={{ fontFamily: 'Trebuchet', color: 'navy', fontStyle: 'italic'}}>I love to visit New York, Paris, and Tokyo.</h1>
 </main>
     )
 }
