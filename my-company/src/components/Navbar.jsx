4c3ad656aa32c4359d58c408162f1c381dc375c8
Fredrick Murefu <freddymurefu@gmail.com>
2025-08-01 16:23:43 +0300

    Fixed Bugs in Navbar.src

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar(){
     return(
-        <nav style={{ padding: '1rem', backgroundColor:'burgundy'  }}>
+        <nav style={{ padding: '1rem', backgroundColor:'burgundy', justifyContent:'center'  }}>
             <ul style={{ listStyle:'none', color:'white', display:'flex', gap:'1rem'}}>
                 <li><Link to ="/">Home</Link></li>
                 <li><Link to="/About">About</Link></li>
