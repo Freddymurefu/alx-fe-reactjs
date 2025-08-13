2ac002c512543524fdd187487037f489268e0d01
Fredrick Murefu <freddymurefu@gmail.com>
2025-08-13 18:33:02 +0300

    Enhanced the User Interface and Added Advanced Search Features

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -2,55 +2,101 @@ import React from 'react'
 import { useState } from 'react'
 import { fetchUserData } from '../services/GithubService'
 
-function Search(){
-    const[username, setUsername] =useState("")
-    const [ user, setUser] = useState(null)
-    const[ loading, setLoading] = useState(false)
-    const[error, setError] = useState(null)
-
+function Search() {
+    const [username, setUsername] = useState("")
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [location, setLocation] = useState("")
+    const [minRepos, setMinRepos] = useState("")
+    const[page, setPage] =useState(1)
+    const[count, setCount]=useState(0)
 
     const handleSubmit = async (event) => {
         event.preventDefault()
         setLoading(true)
         setError(null)
-        setUser(null)
+        
+        // setLocation("")
+        // setMinRepos("")
+        setPage(1)
+        setCount(0)
 
-    try {
-        const data = await fetchUserData(username)
-        setUser(data)
-    } catch (error) {
-       setError(true) 
-    } finally{
-        setLoading(false)
-    }
+        try {
+            console.log({ username, location, minRepos })
+            const data = await fetchUserData(username, location, minRepos, 1)
+            setUsers(data.items)
+            setCount(data.total_count)
+        } catch (error) {
+            setError(true)
+        } finally {
+            setLoading(false)
+        }
 
         console.log("Search for:", username)
     }
-    return(
-        <div>
-        <form onSubmit={handleSubmit}>
-            <input
-            type="text"
-            placeholder="GitHub username"
-            value={username}
-            onChange={(event) =>setUsername(event.target.value)}
-            
-            />
-            <button type="submit">Search</button>
-        </form>
-                {loading && <p>Loading...</p>}
-        {error && <p>Looks like we cant find the user</p>}
 
-        {user && (
-            <div>
-                <img src={user.avatar_url} alt={user.login} width="100"/>
-                <h2>{user.name || user.login}</h2>
-                <a href={user.html_url} target='_blank' rel='noreferrer'>
-                    View Profile
-                </a>
-            </div>
-        )}
-    </div>
+        const loadMore = async() => {
+        const nextPage = page + 1;
+        setPage(nextPage)
+        setLoading(true);
+
+        try {
+        const data = await fetchUserData(username, location, minRepos, nextPage);
+        setUsers(prevUsers => [...prevUsers, ...data.items]); 
+        setPage(nextPage);
+    } catch (err) {
+        setError(true);
+    } finally {
+        setLoading(false);
+    }
+      }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="GitHub username"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Location"
+                    value={location}
+                    onChange={(event) => setLocation(event.target.value)}
+                />
+                <input
+                    type="number"
+                    value={minRepos}
+                    placeholder="Minimum Repos"
+                    onChange={(event) => setMinRepos(event.target.value)}
+                />
+                <button type="submit">Search</button>
+               
+            </form>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>Looks like we cant find the user</p>}
+
+            { users.length > 0 && (
+                <div>
+                    {users.map((u) => (
+                        <div key={u.id}>
+                            <img src={u.avatar_url} alt={u.login} width="100" />
+                            <h2>{u.login}</h2>
+                            <a href={u.html_url} target='_blank' rel='noreferrer'>
+                                View Profile
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            )}
+ {users.length < count &&(
+                    <button onClick={loadMore}>Load More</button>
+                )}
+        </div>
     )
 }
 
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,13 @@
 import axios from "axios"
-export async function fetchUserData(username) {
-    const response = await axios.get(`https://api.github.com/users/${username}`)
-    return response.data
+export async function fetchUserData(username, location, minRepos, page = 1) {
+    
+
+const query = `${username}${location ? `+location:${location}` : ''}${minRepos ? `+repos:>=${minRepos}` : ''}`;
+const response = await axios.get(`https://api.github.com/search/users?q=${query}&per_page=10&page=${page}`);
+
+return{ 
+    items: response.data.items,  
+    total_count: response.data.total_count
+
+}
 }
