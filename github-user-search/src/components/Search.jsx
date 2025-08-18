76c3c3eaf23240a1c888cbc013acbe9e5f319f24
Fredrick Murefu <freddymurefu@gmail.com>
2025-08-18 09:52:52 +0300

    Styled My App with Tailwindcss

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -55,43 +55,47 @@ function Search() {
       }
 
     return (
-        <div className='max-w-3xl mx-auto p-4'>
-            <form onSubmit={handleSubmit}>
+        <div className='max-w-3xl mx-auto p-4 sm:px-6 lg:px-8 py-6'>
+            <form className='flex flex-col gap-4 mb-6 w-full' onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="GitHub username"
                     value={username}
                     onChange={(event) => setUsername(event.target.value)}
+                    className='w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
                 />
                 <input
                     type="text"
                     placeholder="Location"
                     value={location}
                     onChange={(event) => setLocation(event.target.value)}
+                    className='w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
                 />
                 <input
                     type="number"
                     value={minRepos}
                     placeholder="Minimum Repos"
                     onChange={(event) => setMinRepos(event.target.value)}
+                    className='w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
                 />
-                <button type="submit">Search</button>
+                <button className="w-full sm:w-auto bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600-transition"
+                type="submit">Search</button>
                
             </form>
 
-            {loading && <p>Loading...</p>}
-            {error && <p>Looks like we cant find the user</p>}
+            {loading && <p className='text-green-500 mb-4'>Loading...</p>}
+            {error && <p className='text-red-500 mb-4'>Looks like we cant find the user</p>}
 
 
             { users.length > 0 && (
-                <div>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-3 gap-6'>
                     {users.map((u) => (
-                        <div key={u.id}>
-                            <img src={u.avatar_url} alt={u.login} width="100" />
-                            <h2>{u.login}</h2>
-                            <p>Location: {u.location || "Not Available"}</p>
-                            <p>Public Repos: {u.public_repos}</p>
-                            <a href={u.html_url} target='_blank' rel='noreferrer'>
+                        <div className='border-rounded p-4 flex flex-col items-center text-center hover: shadow-lg transition' key={u.id}>
+                            <img src={u.avatar_url} alt={u.login} className='w-50 h-50 sm:h-60 w-60 rounded-full mb-2' />
+                            <h2 className='font-bold text-lg sm:text-xl mb-1'>{u.login}</h2>
+                            <p className='text-sm sm:text-base mb-1'>Location: {u.location || "Not Available"}</p>
+                            <p className='text-sm sm:text-base mb-1'>Public Repos: {u.public_repos}</p>
+                            <a className="text-blue-500 hover:underline" href={u.html_url} target='_blank' rel='noreferrer'>
                                 View Profile
                             </a>
                         </div>
@@ -99,7 +103,7 @@ function Search() {
                 </div>
             )}
  {users.length < count &&(
-                    <button onClick={loadMore}>Load More</button>
+                    <button className='mt-6 w-full sm:w-auto bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition' onClick={loadMore}>Load More</button>
                 )}
         </div>
     )
