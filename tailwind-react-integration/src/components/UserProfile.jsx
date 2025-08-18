f9632eb20f4648ff99af274e32e0f0aa7e388b8b
Fredrick Murefu <freddymurefu@gmail.com>
2025-08-18 17:57:41 +0300

    Implemented Responsive Design With Tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm-[400px] mx-auto my-20 rounded-lg shadow-lg">
-      <img className="rounded-full w-36 h-36 mx-auto" src="https://placehold.co/150" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg">
+      <img className="rounded-full sm:w-24 h-24 md:w-36 h-36 mx-auto" src="https://placehold.co/150" alt="User" />
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
