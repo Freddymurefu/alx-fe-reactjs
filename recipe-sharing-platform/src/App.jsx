335733105c3e4fae54afbc20b0a52114bd30377e
Fredrick Murefu <freddymurefu@gmail.com>
2025-08-19 15:50:03 +0300

    Initial Setup of Recipe Sharing Platform Project

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+      <h1 className='text-blue-500'> Hello, TailwindCss show me you're working!</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
