27e9fe56102bd40b56449cba08cefe67d7add03e
Fredrick Murefu <freddymurefu@gmail.com>
2025-08-19 18:11:59 +0300

    Fixed Routing Issue in App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import HomePage from './components/HomePage'
-import { Routes, Route } from 'react-router-dom';
-import RecipeDetail from './components/RecipeDetail'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
-  
   return (
-   <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path="/recipe/:id" element={<RecipeDetail />} />
-   </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
